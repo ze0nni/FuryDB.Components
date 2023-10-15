4a1cf5b54161d44adbeb90ec7614cd8857f322cc
ze0nni <ze0nni@gmail.com>
2023-10-15 18:12:21 +0300

    AxisConst

---
+++ Runtime/Settings/Keys/Axis.cs
@@ -0,0 +1,36 @@
+namespace FDB.Components.Settings
+{
+    public static class Axis
+    {
+        public const string MouseX = "MouseX";
+        public const string MouseY = "MouseY";
+        public const string JoyX ="JoyX";
+        public const string JoyY = "JoyY";
+        public const string Joy3 = "Joy3";
+        public const string Joy4 = "Joy4";
+        public const string Joy5 = "Joy5";
+        public const string Joy6 = "Joy6";
+        public const string Joy7 = "Joy7";
+        public const string Joy8 = "Joy8";
+        public const string Joy9 = "Joy9";
+        public const string Joy10 = "Joy10";
+        public const string Joy11 = "Joy11";
+        public const string Joy12 = "Joy12";
+        public const string Joy13 = "Joy13";
+        public const string Joy14 = "Joy14";
+        public const string Joy15 = "Joy15";
+        public const string Joy16 = "Joy16";
+        public const string Joy17 = "Joy17";
+        public const string Joy18 = "Joy18";
+        public const string Joy19 = "Joy19";
+        public const string Joy20 = "Joy20";
+        public const string Joy21 = "Joy21";
+        public const string Joy22 = "Joy22";
+        public const string Joy23 = "Joy23";
+        public const string Joy24 = "Joy24";
+        public const string Joy25 = "Joy25";
+        public const string Joy26 = "Joy26";
+        public const string Joy27 = "Joy27";
+        public const string Joy28 = "Joy28";
+    }
+}

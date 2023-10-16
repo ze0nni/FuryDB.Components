f082af0ad906a255cf86500428f50fa57df9ca2f
ze0nni <ze0nni@gmail.com>
2023-10-16 22:08:31 +0300

    Exclude Settings to https://github.com/ze0nni/FuryPersistent

--- Runtime/Navigation/NavigationManager.cs
+++ Runtime/Navigation/NavigationManager.cs
@@ -1,4 +1,3 @@
-using FDB.Components.Settings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,14 +19,6 @@ namespace FDB.Components.Navigation
         internal readonly List<NavigationGroup> _groups = new List<NavigationGroup>();
         internal NavigationGroup _active;
 
-        public delegate ref BindingButton BindingButtonDelegate();
-        BindingButtonDelegate _upBinding;
-        BindingButtonDelegate _downBinding;
-        BindingButtonDelegate _leftBinding;
-        BindingButtonDelegate _rightBinding;
-        BindingButtonDelegate _successBinding;
-        BindingButtonDelegate _cancelBinding;
-
         internal void RegisterGroup(NavigationGroup group)
         {
             _groups.Add(group);
@@ -40,52 +31,8 @@ namespace FDB.Components.Navigation
             _active = _groups.LastOrDefault();
         }
 
-        public void Bind(
-            BindingButtonDelegate up,
-            BindingButtonDelegate down,
-            BindingButtonDelegate left,
-            BindingButtonDelegate right,
-            BindingButtonDelegate success,
-            BindingButtonDelegate cancel
-            )
-        {
-            _upBinding = up;
-            _downBinding = down;
-            _leftBinding = left;
-            _rightBinding = right;
-            _successBinding = success;
-            _cancelBinding = cancel;
-        }
-
         private void Update()
         {
-            if (_active != null)
-            {
-                if (_upBinding != null && _upBinding().CaptureJustPressed())
-                {
-                    Up();
-                }
-                if (_downBinding != null && _downBinding().CaptureJustPressed())
-                {
-                    Down();
-                }
-                if (_leftBinding != null && _leftBinding().CaptureJustPressed())
-                {
-                    Left();
-                }
-                if (_rightBinding != null && _rightBinding().CaptureJustPressed())
-                {
-                    Right();
-                }
-                if (_successBinding != null && _successBinding().CaptureJustPressed())
-                {
-                    Success();
-                }
-                if (_cancelBinding != null && _cancelBinding().CaptureJustPressed())
-                {
-                    Cancel();
-                }
-            }
             if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
             {
                 ClearSelection();
